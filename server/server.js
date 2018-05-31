3eab1bca71667607636cc90da3f2b5dafea4dbdf
Lillian Li <lfli@Lillians-MacBook-Pro.local>
2018-05-31 10:23:03 -0700

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const config=require('./config/config');
 const _=require('lodash');
 const {ObjectID}=require('mongodb');
 
@@ -91,6 +92,21 @@ app.patch('/todos/:id',(req,res)=>{
   }).catch((e)=>{
     res.status(400).send();
   })
+});
+
+app.post('/users',(req,res)=>{
+
+  var body=_.pick(req.body,['email','password']);
+
+  var user=new User(body);
+
+   user.save().then(()=>{
+     return user.generateAuthToken();
+   }).then((token)=>{
+     res.header('x-auth',token).send(user);
+   }).catch((e)=>{
+     res.status(400).send(e);
+   })
 });
 
 app.listen(port,()=>{
